c74f96fe44f22bff432347901e5053877bfceaa6
Christian Rojas <cjrojas72@gmail.com>
2020-07-01 00:23:20 -0400

    data display and filters for data

--- ghostpost_front/src/App.js
+++ ghostpost_front/src/App.js
@@ -1,27 +1,6 @@
 import React from "react";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -39,10 +18,59 @@ class App extends React.Component {
       });
   }
 
+  all_filter = () => {
+    fetch(" http://127.0.0.1:8000/posts/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ posts_data: data });
+      });
+  };
+
+  boast_filter = () => {
+    fetch(" http://127.0.0.1:8000/posts/boast")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ posts_data: data });
+      });
+  };
+
+  roast_filter = () => {
+    fetch(" http://127.0.0.1:8000/posts/roast")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ posts_data: data });
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>hello World</h1>
+        <h1>Ghost Post</h1>
+        <div>
+          <h3>Add a Boast or Roast!</h3>
+          <button>
+            <a href="http://127.0.0.1:8000/addpost/">Post!</a>
+          </button>
+        </div>
+        <div className="filters-div">
+          <button onClick={this.all_filter}>All Posts</button>
+          <button onClick={this.boast_filter}>All Boasts</button>
+          <button onClick={this.roast_filter}>All Roasts</button>
+        </div>
+        {this.state.posts_data.map((post) => {
+          return (
+            <li key={post.id}>
+              <p>choice: {post.choice}</p>
+              <p>{post.body}</p>
+              <p>{post.date_time}</p>
+              <p>Likes:{post.up_votes}</p>
+              <p>dislikes:{post.down_votes}</p>
+            </li>
+          );
+        })}
       </div>
     );
   }
